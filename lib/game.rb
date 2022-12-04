bcee2711237170995c9e14d903eba4d8d7cdc00c
Bwa <40144023+beaesq@users.noreply.github.com>
2022-12-05 00:00:20 +0800

    Adds #move_piece, starts #get_pawn_moves

--- lib/game.rb
+++ lib/game.rb
@@ -24,11 +24,49 @@ class Game
   end
 
   def move_piece
-    
+    move = get_move_input
+    start_coordinates = move[0]
+    end_coordinates = move[1]
+    end_sq = find(end_coordinates[0], end_coordinates[1])
+    start_sq = find(start_coordinates[0], start_coordinates[1])
+    end_sq.piece = start_sq.piece
+    start_sq.piece = nil
+  end
+
+  def get_move_input
+    #aks for input
+    input = gets.chomp
+    raise 'Please enter valid coordinates >:(' unless input.match?(/[0-7]/)
+    raise "That square is empty! you can't move nothing" if empty_square?(start_coordinates)
+    raise 'Please enter a valid move!' unless valid_move?(start_coordinates, end_coordinates)
+  rescue StandardError => e
+    print e.message
+    retry
+  end
+
+  # this assumes that the start square is not empty and the coordinates are within the board
+  def valid_move?(start_coordinates, end_coordinates)
+    start_x = start_coordinates[0]
+    start_y = start_coordinates[1]
+    current_sq = find(start_x, start_y)
+    current_piece = current_sq.piece
+    valid_moves = get_valid_moves(current_piece, start_x, start_y)
+    return true if valid_moves.include?(end_coordinates)
+
+    false
+  end
+
+  def empty_square?(start_coordinates)
+    current_sq = find(start_coordinates[0], start_coordinates[1])
+    return true if current_sq.piece.nil?
+
+    false
   end
 
   def find(x, y, board_array = @board_array)
     return nil if x.nil? || y.nil?
+    return nil if x.negative? || x > 7
+    return nil if y.negative? || y > 7
 
     board_array[x][y]
   end
@@ -52,7 +90,43 @@ class Game
     end
   end
 
+  def get_pawn_moves(x, y, color)
+    moves = []
+    if color == 'white'
+      if y == 1
+        moves.push([x, y + 1], [x, y + 2])
+      else
+        moves.push([x, y + 1])
+      end
+      moves.push([x - 1, y + 1]) unless find(x - 1, y + 1).nil? || find(x - 1, y + 1).piece.nil?
+      moves.push([x + 1, y + 1]) unless find(x + 1, y + 1).nil? || find(x + 1, y + 1).piece.nil?
+      # hello start here (en passant)
+    end
+    if color == 'black'
+      if y == 6
+        moves.push([x, y - 1], [x, y - 2])
+      else
+        moves.push([x, y - 1])
+      end
+      moves.push([x - 1, y - 1]) unless find(x - 1, y - 1).nil? || find(x - 1, y - 1).piece.nil?
+      moves.push([x + 1, y - 1]) unless find(x + 1, y - 1).nil? || find(x + 1, y - 1).piece.nil?
+    end
+    moves
+  end
+
   private
+
+  def get_valid_moves(current_piece, start_x, start_y)
+    case current_piece
+    when '♟︎' then get_pawn_moves(start_x, start_y, 'black')
+    when '♙' then get_pawn_moves(start_x, start_y, 'white')
+    when '♚', '♔' then get_king_moves(start_x, start_y)
+    when '♛', '♕' then get_queen_moves(start_x, start_y)
+    when '♞', '♘' then get_knight_moves(start_x, start_y)
+    when '♝', '♗' then get_bishop_moves(start_x, start_y)
+    when '♜', '♖' then get_rook_moves(start_x, start_y)
+    end
+  end
 
   def get_non_pawn_token(x, y)
     if y.zero?
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -29,6 +29,154 @@ describe Game do
       end
       expect(pieces).to all(eq(nil))
     end
+  end
 
+  describe '#move_piece' do
+    context 'when the move is valid' do
+      before do
+        board_array = new_game.instance_variable_get(:@board_array)
+        board_array[1][1].piece = '♖'
+        new_game.instance_variable_set(:@board_array, board_array)
+        allow(new_game).to receive(:get_move_input).and_return([[1, 1], [3, 4]])
+      end
+
+      it 'moves the piece to the correct square' do
+        new_game.move_piece
+        board_array = new_game.instance_variable_get(:@board_array)
+        start_sq = board_array[1][1]
+        end_sq = board_array[3][4]
+        expect(start_sq.piece).to be_nil
+        expect(end_sq.piece).to eq('♖')
+      end
+    end
+  end
+
+  describe '#valid_move?' do
+    context 'when the move is valid' do
+      before do
+        board_array = new_game.instance_variable_get(:@board_array)
+        board_array[1][1].piece = '♔'
+        new_game.instance_variable_set(:@board_array, board_array)
+        allow(new_game).to receive(:get_valid_moves).and_return([[0, 1], [1, 0], [2, 1], [1, 2]])
+      end
+      it 'returns true' do
+        start_coordinates = [1, 1]
+        end_coordinates = [1, 2]
+        result = new_game.valid_move?(start_coordinates, end_coordinates)
+        expect(result).to be true
+      end
+    end
+
+    context 'when the move is invalid' do
+      before do
+        board_array = new_game.instance_variable_get(:@board_array)
+        board_array[1][1].piece = '♔'
+        new_game.instance_variable_set(:@board_array, board_array)
+        allow(new_game).to receive(:get_valid_moves).and_return([[0, 1], [1, 0], [2, 1], [1, 2]])
+      end
+      it 'returns false' do
+        start_coordinates = [1, 1]
+        end_coordinates = [1, 4]
+        result = new_game.valid_move?(start_coordinates, end_coordinates)
+        expect(result).to be false
+      end
+    end
+  end
+
+  describe '#empty_square?' do
+    context 'when the square is empty' do
+      it 'returns true' do
+        result = new_game.empty_square?([1, 1])
+        expect(result).to be true
+      end
+    end
+    context 'when the square is not empty' do
+      before do
+        board_array = new_game.instance_variable_get(:@board_array)
+        board_array[1][1].piece = '♖'
+        new_game.instance_variable_set(:@board_array, board_array)
+      end
+      it 'returns false' do
+        result = new_game.empty_square?([1, 1])
+        expect(result).to be false
+      end
+    end
+  end
+
+  describe '#get_pawn_moves' do
+    context 'when there is no enemy piece diagonal to the pawn' do
+      it 'returns the correct moves from the starting square' do
+        start_coordinates = [1, 6]
+        color = 'black'
+        valid_moves = new_game.get_pawn_moves(start_coordinates[0], start_coordinates[1], color)
+        expect(valid_moves).to match_array([[1, 5], [1, 4]])
+      end
+      it 'returns the correct moves from a non-starting square' do
+        start_coordinates = [1, 4]
+        color = 'white'
+        valid_moves = new_game.get_pawn_moves(start_coordinates[0], start_coordinates[1], color)
+        expect(valid_moves).to match_array([[1, 5]])
+      end
+    end
+    context 'when there is an enemy piece diagonal to the pawn' do
+      it 'returns the correct moves from a starting square' do
+        board_array = new_game.instance_variable_get(:@board_array)
+        board_array[1][2].piece = '♔'
+        new_game.instance_variable_set(:@board_array, board_array)
+        start_coordinates = [0, 1]
+        color = 'white'
+        valid_moves = new_game.get_pawn_moves(start_coordinates[0], start_coordinates[1], color)
+        expect(valid_moves).to match_array([[0, 2], [0, 3], [1, 2]])
+      end
+      it 'returns the correct moves from a non-starting square' do
+        board_array = new_game.instance_variable_get(:@board_array)
+        board_array[6][4].piece = '♔'
+        new_game.instance_variable_set(:@board_array, board_array)
+        start_coordinates = [7, 5]
+        color = 'black'
+        valid_moves = new_game.get_pawn_moves(start_coordinates[0], start_coordinates[1], color)
+        expect(valid_moves).to match_array([[7, 4], [6, 4]])
+      end
+    end
+    context 'when an en passant is possible' do
+      xit 'returns the correct moves' do
+      
+      end
+    end
+  end
+
+  describe '#get_king_moves' do
+    xit 'returns the correct moves' do
+      
+    end
+    context 'when castling is possible' do
+      xit 'returns the correct moves' do
+        # checks if rook is still in original square
+      end
+    end
+  end
+
+  describe '#get_queen_moves' do
+    xit 'returns the correct moves' do
+      
+    end
+  end
+
+  describe '#get_knight_moves' do
+    xit 'returns the correct moves' do
+      
+    end
+  end
+
+  describe '#get_bishop_moves' do
+    xit 'returns the correct moves' do
+      
+    end
+  end
+
+  describe '#get_rook_moves' do
+    xit 'returns the correct moves' do
+      
+    end
   end
 end
